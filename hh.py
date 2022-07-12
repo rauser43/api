ff7515adef8e6cccc557eff5c2464bdf77e86e50
newtask <irina.fedorova31@gmail.com>
2022-07-12 21:26:24 +0300

    Page

--- hh.py
+++ hh.py
@@ -20,3 +20,22 @@ result = requests.get(url_vacancies, params=params).json()
 # pprint.pprint(first)
 # print (first ['alternative_url'])
 # one_vacancy_url = first ['url']
+
+import requests
+import pprint
+
+url = 'https://api.hh.ru/vacancies'
+
+where = input('Где искать вакансию?')
+query_string = input('Строка запроса?')
+
+params = {
+    'text': 'NAME:(Python OR Java) AND COMPANY_NAME:(1 OR 2 OR YANDEX) AND (DJANGO OR SPRING)',
+    'page': 1
+}
+
+result = requests.get(url, params=params).json()
+
+pprint.pprint(result)
+print(result['items'][0]['url'])
+print(result['items'][0]['alternate_url'])
